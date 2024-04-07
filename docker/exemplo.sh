70bf76cc17c2e9442ac401ce0c2bc5194593349d
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2024-04-07 14:53:57 -0300

    Pequenos ajustes de diretório

--- docker/exemplo.sh
+++ docker/exemplo.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/kyriosdata/ig
 cd ig
-chmod +x /ig/*.sh
+chmod +x *.sh
 ./_updatePublisher.sh --yes
 ./_genonce.sh --yes
-cp -r /ig/output/. /var/www/html/
+cp -r output/. /./var/www/html/
